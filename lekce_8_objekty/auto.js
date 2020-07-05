f633058a6d67581849f14e99a06a74cf74cc2803
dn8 <daniel8cech@gmail.com>
2020-07-05 20:27:16 +0200

    autosaved and pushed by git

--- lekce_8_objekty/auto.js
+++ lekce_8_objekty/auto.js
@@ -30,7 +30,7 @@ let auto = {
 
     natankuj(litru) {
         if(this.nadrz + litru > this.kapacitaNadrze) {
-            console.log(`To je moc. Natankovano na plnou (${ this.kapacitaNadrze } l).`)
+            console.log(`To je moc. Natankovano na plnou (${ this.kapacitaNadrze - this.nadrz } l).`)
             this.nadrz = this.kapacitaNadrze
         } else {
             this.nadrz += litru
